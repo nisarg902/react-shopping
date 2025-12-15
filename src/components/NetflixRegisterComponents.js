a15ea20e56c3b4c2b71375ccd49eb1dd2ce47908
Nisarg Wanve <nisargwanve1@gmail.com>
2025-12-14 21:11:32 -0800

    auto update

--- src/components/NetflixRegisterComponents.js
+++ src/components/NetflixRegisterComponents.js
@@ -1,7 +1,7 @@
 export default function NetflixRegisterComponents() {
   return <div className="fluid-Container text-center mt-3">
     
-    are youeitht ready?
+  Ready to watch? Enter your email to create or restart your membership.
        <div>
          <input type="email"></input>
          <button className="btn btn-danger m-3">GET started </button>
