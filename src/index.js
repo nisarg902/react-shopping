63216468b21cc5f7fdc7714063039ca9c5b99d61
Nisarg Wanve <nisargwanve1@gmail.com>
2025-12-14 20:52:57 -0800

    auto update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import '../node_modules/bootstrap/dist/bootstrap.css';
-import '../node_modules/bootstrap-icons'
+import '../node_modules/bootstrap-icons/'
 import NetflixRegisterComponents from './components/NetflixRegisterComponents';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
