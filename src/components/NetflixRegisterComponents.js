e2744d4442b5538e2645cbfc70cf6fc1ccb3291f
Nisarg Wanve <nisargwanve1@gmail.com>
2025-12-14 21:01:27 -0800

    auto update

--- src/components/NetflixRegisterComponents.js
+++ src/components/NetflixRegisterComponents.js
@@ -1,5 +1,5 @@
 export default function NetflixRegisterComponents() {
-  return <div className="fluid-Container">
+  return <div className="fluid-Container text-center">
     
     are youeitht ready?
     </div>;
