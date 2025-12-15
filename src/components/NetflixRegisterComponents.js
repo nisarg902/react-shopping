833153e7dd10c2a1a9f194ac62872ccb9d55b47b
Nisarg Wanve <nisargwanve1@gmail.com>
2025-12-14 21:05:26 -0800

    auto update

--- src/components/NetflixRegisterComponents.js
+++ src/components/NetflixRegisterComponents.js
@@ -2,6 +2,9 @@ export default function NetflixRegisterComponents() {
   return <div className="fluid-Container text-center mt-3">
     
     are youeitht ready?
-     
+       <div>
+         <input type="email"></input>
+
+       </div>
     </div>;
 }
