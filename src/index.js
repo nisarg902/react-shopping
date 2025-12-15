14bd70ac407399275d6e1ead6827d79adfeae28a
Nisarg Wanve <nisargwanve1@gmail.com>
2025-12-14 20:52:33 -0800

    auto update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import '../node_modules/bootstrap/dist/bootstrap.css';
+imp
 
 import NetflixRegisterComponents from './components/NetflixRegisterComponents';
 
