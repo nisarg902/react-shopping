49ad8cabbd49f973bac23a0b3d60bb1cf9b8c995
Nisarg Wanve <nisargwanve1@gmail.com>
2025-12-14 21:10:25 -0800

    auto update

--- src/components/NetflixRegisterComponents.js
+++ src/components/NetflixRegisterComponents.js
@@ -4,7 +4,8 @@ export default function NetflixRegisterComponents() {
     are youeitht ready?
        <div>
          <input type="email"></input>
-         <button className="btn btn-danger">GET started </button>
+         <button className="btn btn-danger" cl
+         >GET started </button>
             
        </div>
     </div>;
