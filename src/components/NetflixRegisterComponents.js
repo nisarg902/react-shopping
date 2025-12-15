29cf3991b55b119d6f3da7ef3bb91f9086b245e8
Nisarg Wanve <nisargwanve1@gmail.com>
2025-12-14 21:12:35 -0800

    auto update

--- src/components/NetflixRegisterComponents.js
+++ src/components/NetflixRegisterComponents.js
@@ -1,6 +1,7 @@
 export default function NetflixRegisterComponents() {
   return <div className="fluid-Container text-center mt-3">
-       < h3
+       <h3> Unlimited movies, shows, and more</h3
+
   Ready to watch? Enter your email to create or restart your membership.
       < p>Starts at ₹149. Cancel at any time.</p>
        <div>
