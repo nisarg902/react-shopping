827e448c8f798f2f9505b37270510a0ee2110aba
Nisarg Wanve <nisargwanve1@gmail.com>
2025-12-14 20:27:09 -0800

    auto update

---
+++ myapp/src/App.js
@@ -0,0 +1,9 @@
+import { NetflixRegisterComponent } from "./components/NetflixRegisterComponent";
+
+function App() {
+  return (
+    <NetflixRegisterComponent />
+  );
+}
+
+export default App;
---
+++ src/components/AmazonComponents.js
@@ -0,0 +1,3 @@
+export default function AmazonComponents() {
+  return <h2>are you ready?</h2>;
+}
